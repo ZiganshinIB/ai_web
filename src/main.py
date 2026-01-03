25e4afc9a620f9a2c36e51d0158664f9b113780d
Ильмир Зиганшин <elzig3012@yandex.ru>
2026-01-03 09:12:38 +0300

    Connect to frontend

--- src/main.py
+++ src/main.py
@@ -1,3 +1,34 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# Ваши маршруты
+router = APIRouter()
+
+
+# Пример маршрута
+@router.get("/test")
+async def index():
+    return {"message": "Hello World"}
+
+
+app.include_router(router)
+
+# Подключение фронтенда
+FRONTEND_DIST = Path(__file__).parent.parent / "frontend"
+# Статика (js, css, assets)
+app.mount(
+    "/assets",
+    StaticFiles(directory=FRONTEND_DIST / "assets"),
+    name="assets",
+)
+
+
+# SPA fallback (React Router)
+@app.get("/{full_path:path}")
+def serve_react_app(full_path: str):
+    return FileResponse(FRONTEND_DIST / "index.html")
